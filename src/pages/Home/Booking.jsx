b51aa4ce72ac943a38bcb6db17f9bcc1e762dc62
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-03 00:46:21 +0600

    landing page is almost done

---
+++ src/pages/Home/Booking.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../../assets/New folder/bookingIcon.png'
+
+const Booking = () => {
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        fetch('boking.json')
+            .then(res => res.json())
+            .then(data => setBooks(data))
+    })
+    return (
+        <div>
+            <h1 className='text-3xl font-bold text-[#03373D] translate-x-6'>How It Works?</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 bg-[#f8fafc] rounded-xl">
+                {books.map((book, index) => (
+                    <div
+                        key={index}
+                        className="flex flex-col items-center text-center bg-white shadow-md rounded-xl p-5 hover:shadow-xl hover:-translate-y-2 transition-all duration-300"
+                    >
+                        <img
+                            src={logo}
+                            className="w-20 h-20 object-contain mb-4"
+                        />
+                        <h1 className="text-[#03373D] text-xl font-bold mb-2">
+                            {book.title}
+                        </h1>
+                        <p className="text-sm text-gray-500">{book.text}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    );
+};
+
+export default Booking;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 
 import Banner from '../Banner/Banner';
+import Booking from '../Booking';
 import Services from '../Services';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <Booking></Booking>
            <Services></Services>
         </div>
     );
