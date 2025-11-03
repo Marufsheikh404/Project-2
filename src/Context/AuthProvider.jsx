7dca8ad97a26d8d3e5fe5c211b9483f4768635a0
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-03 22:07:25 +0600

    current user is set

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from '../firebase/firebase.init'
 
 const AuthProvider = ({children}) => {
-    const [users, serUsers] =useState(null);
+    const [users, setUsers] =useState(null);
     const [loading,setLoading] = useState(false)
     
     const createUser =(email,password)=>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth,email,password)
     };
 
-    
+    const signIn =(email, password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email,password)
+    };
+
+    useEffect(()=>{
+        const unsubcribe = onAuthStateChanged((auth, currentUser)=>{
+            console.log('CurrentUser', currentUser);
+            setUsers(currentUser);
+        })
+        return()=>{
+            unsubcribe()
+        }
+    });
     const userInfo ={
         loading,
         setLoading,
-        createUser
+        createUser,
+        signIn
     }
     return (
         <AuthContext.Provider value={userInfo}>
