6caa7203bce0ca1dfedf941c42fea91c4ece7532
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-03 16:05:41 +0600

    login page is running

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Layout/Root";
 import Home from "../pages/Home/Home/Home";
+import AuthLayout from "../Layout/AuthLayout";
+import Login from "../pages/Authentication/Login/Login";
 
 const Router = createBrowserRouter([
     {
@@ -10,6 +12,16 @@ const Router = createBrowserRouter([
             {
                 index: true,
                 Component: Home
+            }
+        ]
+    },
+    {
+        path:'/',
+        Component: AuthLayout,
+        children:[
+            {
+                path:'login',
+                Component: Login
             }
         ]
     }
