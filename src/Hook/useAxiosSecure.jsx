02821858d8ed56c126ab8424e0ac7bf5be7430d6
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-07 12:22:59 +0600

    access token

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -1,9 +1,17 @@
 import axios from 'axios';
+import useAuth from './useAuth';
 
 const AxiosSecure = axios.create({
     baseURL:'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    const {users}= useAuth();
+    AxiosSecure.interceptors.request.use(config=>{
+        config.headers.Authorization = `Bearer${users.accessToken}`
+        return config;
+    }, error=>{
+        Promise.reject(error)
+    })
     return AxiosSecure;
 }
 export default useAxiosSecure;
