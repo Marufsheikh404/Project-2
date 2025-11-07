3a2c2ba375e90f182bd343b53c272a6386c23ae5
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-08 01:16:57 +0600

    private route done

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -1,16 +1,37 @@
 import axios from 'axios';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const AxiosSecure = axios.create({
     baseURL:'http://localhost:5000'
 })
 const useAxiosSecure = () => {
-    const {users}= useAuth();
+    const navigate = useNavigate();
+    const {users, SignOut}= useAuth();
     AxiosSecure.interceptors.request.use(config=>{
         config.headers.Authorization = `Bearer ${users.accessToken}`
         return config;
     }, error=>{
         Promise.reject(error)
+    })
+
+    AxiosSecure.interceptors.response.use(res=>{
+        return res;
+    }, error =>{
+        const status = error.status;
+        if(status === 403){
+            navigate('/forbidden')
+        }
+        else if(status === 401){
+             SignOut()
+             .then(()=>{
+                navigate('/login')
+             })
+             .catch((error=>{
+                console.log(error)
+             }))
+        }
+        return Promise.reject(error)
     })
     return AxiosSecure;
 }
